25740b5a183d0aa8d2b1dc03a1379924cf86fbc6
Pavithra <pavithrakasukurthi00@gmail.com>
2026-08-29 16:07:29 +0530

    eks

--- 30-bastion/main.tf
+++ 30-bastion/main.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "bastion" {
     instance_type = var.instance_type
     vpc_security_group_ids = [local.bastion_sg_id]
     subnet_id = local.public_subnet_id
-    iam_instance_profile = aws_iam_instance_profile.bastion.name
 
     root_block_device {
         volume_size = 50
